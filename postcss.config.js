d2939cde9d7d7f3dc96bd35a13b00d8c87173e66
Barret Rennie <barret@brennie.ca>
2017-08-12 01:55:17 -0600

    Add production & development configurations

--- postcss.config.js
+++ postcss.config.js
@@ -3,11 +3,17 @@
 const path = require('path');
 
 
+const plugins = [
+  require('postcss-import')({
+    root: path.resolve(__dirname),
+    path: path.resolve(__dirname, 'src'),
+  }),
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(require('cssnano'));
+}
+
 module.exports = {
-  plugins: [
-    require('postcss-import')({
-      root: path.resolve(__dirname),
-      path: path.resolve(__dirname, 'src'),
-    }),
-  ],
+  plugins,
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,39 @@
 
 const path = require('path');
 
+const BabiliPlugin = require('babili-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
 const ScriptExtPlugin = require('script-ext-html-webpack-plugin');
+const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProduction = nodeEnv === 'production';
+
+const hashify = filename => {
+  if (!isProduction) {
+    return filename;
+  }
+
+  const ext = path.extname(filename);
+  const basename = path.basename(filename, ext);
+
+  return `${basename}.[hash]${ext}`;
+};
 
 const vendorCss = new ExtractTextPlugin({
-  filename: path.join('static', 'css', 'vendor.css'),
+  filename: path.join('static', 'css', hashify('vendor.css')),
 });
 
 const appCss = new ExtractTextPlugin({
-  filename: path.join('static', 'css', 'tapdown.css'),
+  filename: path.join('static', 'css', hashify('tapdown.css')),
 });
 
-module.exports = {
+const htmlTemplatePath = path.resolve(__dirname, 'src', 'html', 'index.ejs');
+
+const commonConfig = {
   devtool: 'source-map',
   entry: [
     path.resolve(__dirname, 'src', 'js', 'index.jsx'),
@@ -73,7 +93,7 @@ module.exports = {
           options: {
             outputPath: `${path.join('static', 'font')}${path.sep}`,
             publicPath: '../font/',
-            name: '[name].[ext]',
+            name: hashify('[name].[ext]'),
             useRelativePath: true,
           },
         },
@@ -85,18 +105,53 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: `'${nodeEnv}'`,
+      },
+    }),
     appCss,
     vendorCss,
-    new HtmlPlugin({
-      inject: 'head',
-      template: path.resolve(__dirname, 'src', 'html', 'index.ejs')
-    }),
-    new ScriptExtPlugin({
-      defaultAttribute: 'defer',
-    }),
   ],
   resolve: {
     extensions: ['.js', '.jsx'],
     modules: [path.resolve(__dirname, 'src'), 'node_modules'],
   },
 };
+
+const productionConfig = () => ({
+  plugins: [
+    HtmlPlugin({
+      inject: 'head',
+      minify: {
+        collapseInlineTagWhitespace: true,
+        collapseWhitespace: true,
+        quoteCharacter: '"',
+      },
+      template: htmlTemplatePath,
+    }),
+    new BabiliPlugin(),
+  ],
+});
+
+const developmentConfig = () => ({
+  plugins: [
+    new HtmlPlugin({
+      inject: 'head',
+      template: htmlTemplatePath,
+    }),
+  ],
+});
+
+
+module.exports = webpackMerge(
+  commonConfig,
+  isProduction ? productionConfig() : developmentConfig(),
+  {
+    plugins: [
+      new ScriptExtPlugin({
+        defaultAttribute: 'defer',
+      }),
+    ],
+  }
+);
