33451cbf103407160f7e7c0839616b38accccd14
Barret Rennie <barret@brennie.ca>
2017-08-13 15:31:40 -0600

    Add hover highlight on new game button

--- postcss.config.js
+++ postcss.config.js
@@ -7,7 +7,9 @@ const plugins = [
     root: path.resolve(__dirname),
     path: path.resolve(__dirname, 'src'),
   }),
+  require('postcss-nested'),
   require('postcss-simple-vars'),
+  require('postcss-color-function'),
 ];
 
 if (process.env.NODE_ENV === 'production') {
