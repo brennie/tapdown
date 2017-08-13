1f3dbb386402832afbaf680f98e7c27604063e3a
Barret Rennie <barret@brennie.ca>
2017-08-13 15:30:53 -0600

    Refactor CSS to use vars for common colours

--- postcss.config.js
+++ postcss.config.js
@@ -7,10 +7,11 @@ const plugins = [
     root: path.resolve(__dirname),
     path: path.resolve(__dirname, 'src'),
   }),
+  require('postcss-simple-vars'),
 ];
 
 if (process.env.NODE_ENV === 'production') {
-  plugins.push(require('cssnano'));
+  plugins.push(require('cssnano')());
 }
 
 module.exports = {
