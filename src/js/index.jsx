478ec0d7a2a22b04a3e747645939a7fecba6e130
Barret Rennie <barret@brennie.ca>
2017-08-13 04:02:39 -0600

    Add redux, react-redux, react-router, react-router-redux

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createHistory from 'history/createMemoryHistory';
+import { Route } from 'react-router';
+import {
+  ConnectedRouter,
+  routerMiddleware,
+  routerReducer,
+} from 'react-router-redux';
 
 import Splash from 'js/components/splash';
 
 document.addEventListener('DOMContentLoaded', function onLoad() {
-  render(<Splash />, document.querySelector('.content'));
+  const history = createHistory();
+  const store = createStore(
+    combineReducers({
+      router: routerReducer,
+    }),
+    applyMiddleware(routerMiddleware(history)),
+  );
+
+  render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Route path="/" component={Splash} />
+      </ConnectedRouter>
+    </Provider>,
+    document.querySelector('.content'),
+  );
 });
